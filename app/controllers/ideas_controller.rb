0058fa597d496fc968cdd5fa1649e9a83f0d7963
karry33 <karry33w@gmail.com>
2021-03-22 21:38:01 +0900

    シリアライザーの実装

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,37 @@
+class IdeasController < ApplicationController
+
+
+
+  def create
+    # 1. カテゴリをDBから検索
+    @category = Category.find_by(name: params[:category_name])
+
+    # 2. カテゴリがなければ、カテゴリを作成（Category.new or Category.create）
+    if @category.nil?
+      @category_created = Category.create(name: params[:category_name])
+      Idea.create(category_id: @category_created.id, body: params[:body])
+    else
+      # Idea.create(id: （自動で決まる）,category_id: @category.id, body: params[:body])
+      Idea.create(category_id: @category.id, body: params[:body])
+    end
+
+    # 3. ideaをcreate
+
+    render status: 201
+  end
+
+
+  def index
+    if params[:category_name].present?
+      @category = Category.find_by(name: params[:category_name])
+      @ideas = Idea.find_by(category_id: @category.id)
+    else
+      @ideas = Idea.all
+    end
+    render json: {data: ActiveModelSerializers::SerializableResource.new(@ideas, each_serializer: IdeaSerializer)}
+  end
+
+  # def index_json(ideas)
+  #   (ideas, each_serializer: IdeaSerializer)
+  # end
+end
